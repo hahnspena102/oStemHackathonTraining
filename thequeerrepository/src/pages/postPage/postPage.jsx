6f4b7c4c545bb659c1d69b18ad9f376ce2a5fe93
hahns_pena <hpena02@calpoly.edu>
2022-11-05 09:26:23 -0700

    Unique Post

---
+++ thequeerrepository/src/pages/postPage/postPage.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import SideBar from "../../components/sidebar/sidebar"
+import UniquePost from "../../components/uniquePost/uniquePost"
+import "./postPage.css"
+
+export default function PostPage() {
+    return (
+        <>
+            <div className="postPage">
+                <SideBar/>
+                <UniquePost/>
+            </div>
+        </>
+    )
+}
